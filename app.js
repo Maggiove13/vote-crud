64e63d6c11c9db8c71d053e30242d646b824e133
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-30 14:58:19 -0300

    fix: sintaxis

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ dotenv.config()
 
 const app = express();
 
-
-
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server running in port ${port}`);
+    console.log(`Server running in port: ${port}`);
 });
