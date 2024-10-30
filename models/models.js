f94e512e22a6d8211cb7bce1ffcf8c7f9aba51b4
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-10-30 14:56:18 -0300

    add function verify database connection

--- models/models.js
+++ models/models.js
@@ -1,2 +1,20 @@
 const pool = require("../config/config.js");
 
+
+// Función asíncrona para verificar la conexión a la base de datos
+async function checkDatabaseConnection() {
+    try {
+        const connection = await pool.getConnection();
+        if (!connection) {
+            console.log("There is no connection to the database");
+            return;
+        }
+        console.log("Connection to the MySQL database established successfully");
+        connection.release(); // Libera la conexión después de verificar
+    } catch (error) {
+        console.error("Error connecting to the MySQL database:", error);
+    }
+}
+
+
+module.exports = { checkDatabaseConnection };
