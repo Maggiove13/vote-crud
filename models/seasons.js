60e136a85a04abe358b6fa6e9c05fda71dae9b88
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-01 16:34:24 -0300

    feat: function queryToCreateSeasonTheme

--- models/seasons.js
+++ models/seasons.js
@@ -2,3 +2,16 @@ const { checkDatabaseConnection } = require("./models.js");
 const pool = require("../config/config.js");
 
 checkDatabaseConnection();
+
+
+exports.queryToCreateASeasonTheme = async (id_serie, user_id, season_name) => {
+    const query = 'INSERT INTO seasons (serie_id, user_id, season_name) VALUES = ?, ?, ?';
+
+    try{
+        const [response] = await pool.execute(query, [id_serie, user_id, season_name]);
+        console.log("Data inserted correctly");
+        return response;
+    } catch (error) {
+        console.log("Data could not be inserted. Error:", error);
+    }
+}
