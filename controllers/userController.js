e51b88618f48dfb08295d6fb81a003d7172a8631
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-02 10:46:47 -0300

    feat: to register a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,20 @@
 const { queryToInsertUser, queryToVerifyUser, queryGetUserId } = require("../models/users");
 
+
+exports.createUser = async (req, res) => {
+    const { user_name, email, password } = req.body; 
+
+        if (!user_name || !email || !password){
+            return res.status(400).send({status: "Incomplete", message: "All the fields must be completed"});
+        }
+
+    try{
+
+        await queryToInsertUser(user_name, password, email);
+            return res.status(201).send({message: "User successfully created"})
+
+    } catch(error){
+        res.status(500).send({message: "Error creating user"});
+        console.log("Error creating user", error);
+    }
+}
