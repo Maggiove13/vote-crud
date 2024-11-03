9f3c464f076dcfe0bc4e110f7c6078769d1273c1
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-03 19:58:30 -0300

    feat: InsertSerie controller

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -2,13 +2,33 @@ const { queryToInsertSeriesName, queryVerifySeriesTitle, queryToGetAllTitles, qu
 
 const { getUserId } = require("./userController.js");
 
-exports.InsertSerie = async () => {
 
+exports.InsertSerie = async (req, res) => {
+    const { title, description, user_id } = req.body;
 
-    try{
+    try{  
+        
+        if (!title || !user_id){
+            console.log("title not found");
+            return res.status(400).send({status: "Error", message: "Title and user_id are required"});
+        }
+        
+        const response = await queryToInsertSeriesName(title, description || null, user_id);
+        if (response.affectedRows === 0) {
+            return res.status(201).send({
+                status: "Error inserting data",
+                message: "Data not inserted in the table"
+            });
+        }
 
+        return res.status(201).send({
+            status: "Success",
+            message: "Serie successfully inserted", 
+            data: {title, description, user_id}
+        });
+        
     } catch (error){
-        console.log("Error inserting a serie", error);
+        console.log("Error inserting a serie:", error);
+        res.status(500).send({message: "Internal server error"});
     }
-
 }
