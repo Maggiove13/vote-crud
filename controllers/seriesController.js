b500767871fe83ac3d12a69ef2637b7f27b9c8a3
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-03 20:17:29 -0300

    modify: feat InsertSerie controller to verify title before inserting to the database

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -3,7 +3,7 @@ const { queryToInsertSeriesName, queryVerifySeriesTitle, queryToGetAllTitles, qu
 const { getUserId } = require("./userController.js");
 
 
-exports.InsertSerie = async (req, res) => {
+exports.insertSerie = async (req, res) => {
     const { title, description, user_id } = req.body;
 
     try{  
@@ -13,8 +13,14 @@ exports.InsertSerie = async (req, res) => {
             return res.status(400).send({status: "Error", message: "Title and user_id are required"});
         }
         
-        const response = await queryToInsertSeriesName(title, description || null, user_id);
-        if (response.affectedRows === 0) {
+        const verifyTitleResponse = await queryVerifySeriesTitle(title);
+        if (verifyTitleResponse.length > 0){
+            return res.status(400).send({message: "this title already exists"});
+        }
+
+        const insertResponse = await queryToInsertSeriesName(title, description || null, user_id);
+        console.log(insertResponse);
+        if (insertResponse.affectedRows === 0) {
             return res.status(201).send({
                 status: "Error inserting data",
                 message: "Data not inserted in the table"
@@ -26,7 +32,7 @@ exports.InsertSerie = async (req, res) => {
             message: "Serie successfully inserted", 
             data: {title, description, user_id}
         });
-        
+
     } catch (error){
         console.log("Error inserting a serie:", error);
         res.status(500).send({message: "Internal server error"});
