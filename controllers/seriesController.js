61ad137db8b23c63d1aa9665daa4695935df0363
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-07 16:27:54 -0300

    Rename queryGetSerieById the function name was updated to better reflect its purpose and improve code readability

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -9,11 +9,13 @@ const {
     queryUpdateLink, 
     queryVoteCount, 
     queryGetIdsFromTitle,
-    queryGetSerieById} = require("../models/series.js");
+    querySerieById} = require("../models/series.js");
+
+    const userController = require('../models/users.js');
 
 exports.insertSerie = async (req, res) => {
-    const { title, description, user_id, image, link_url } = req.body;
-
+    const { title, description, image, link_url } = req.body;
+    
     try{  
         
         if (!title || title.trim() === ""){
@@ -21,7 +23,7 @@ exports.insertSerie = async (req, res) => {
             return res.status(400).send({status: "Error", message: "A title is required"});
         }
 
-        const titleLower = title.trim().toLowerCase();
+        const titleUpper = title.trim().toUpperCase();
         const newDescription = description?.trim() || null;
         const newImage = image?.trim() || null;
         const newLink = link_url?.trim() || null;
@@ -31,7 +33,7 @@ exports.insertSerie = async (req, res) => {
             return res.status(400).send({message: "Title already exists"});
         }
         
-        const { serie_id, affectedRows } = await queryToInsertSeriesName(titleLower, newDescription, user_id, newImage, newLink);
+        const { serie_id, affectedRows } = await queryToInsertSeriesName(titleUpper, newDescription, newImage, newLink);
         console.log(affectedRows);
         if (affectedRows === 0) {
             return res.status(400).send({
@@ -94,134 +96,38 @@ exports.deleteSerie = async (req, res) => {
 
 
 exports.updateSerie = async (req, res) => {
+    const { title, description, image, link_url} = req.body;
+    const { serie_id } = req.params;
+    
 
-    const { title, description, image, link, serie_id } = req.body;
+    console.log("Datos recibidos:", req.body); 
+    console.log("Serie ID:", serie_id); 
 
     try{
-
-        if (!title || !title.trim() === ""){
+        if (!title || title.trim() === ""){
             return res.status(400).send({message: "Title not found"});
         }
 
-        const titleLower = title.trim()
+        if (!serie_id){
+            return res.status(400).send({message: "serie_id not found"});
+        }
+
+        const titleUpper = title.trim().toUpperCase();
         const newDescription = description?.trim() || null;
         const newImage = image?.trim() || null;
-        const newLink = link?.trim() || null;
+        const newLink = link_url?.trim() || null;
 
-
-        response = await queryUpdateSerie(titleLower, newDescription, newImage , newLink, serie_id);
+        const response = await queryUpdateSerie(titleUpper, newDescription, newImage, newLink, serie_id);
         if (response.affectedRows === 0){
-            return res.status(400).send({message: "Serie could not be updated"})
+            return res.status(400).send({message: "Serie could not be updated"});
         } else {
             res.redirect('/api/series');
         }
-        
     } catch(error){
         console.error("Error during the update:", error);
         return res.status(500).send({message: "Internal server error"});
     }
 }
-
-
-exports.insertSerieLink = async (req, res) => {
-    const { serie_id, link } = req.body;
-
-    if (!serie_id){
-        return res.status(400).send({message: "serie_id not found"});
-    }
-    
-    const Linkk = link.trim();
-
-    try{
-        if (!Linkk){
-            return res.status(400).send({message: "No link provided"});
-        }
-
-
-        const responseQuery = await queryInsertIntoSeriesLink(Linkk, serie_id);
-        if (responseQuery.affectedRows === 0){
-            return res.status(400).send({message: "The link could not be inserted"});
-        } else {
-            return res.status(200).send({status: "Success", message: "The link was inserted correctly", SerieData: {
-                id: serie_id,
-                Link: Linkk
-            }});
-        }
-
-    } catch(error){
-        console.error("Error insertando el link", error);
-        return res.status(500).send({message: "Internal server error"});
-    }
-
-}
-
-
-
-exports.deleteLink = async (req, res) => {
-    const { serie_id } = req.body;
-
-    if (!serie_id){
-        return res.status(400).send({message: "serie_id is required to delete a link"});
-    }
-    try{
-        
-        const responseQuery = await queryDeleteLink(serie_id);
-
-        if (responseQuery.affectedRows === 0){
-            return res.status(400).send({message: "The Link was not deleted"});
-        } else {
-            return res.status(200).send({message: "The Link successfully deleted"});
-        }
-    } catch(error){
-        console.error("Error found:", error);
-        return res.status(500).send({message: "Internal server error"});
-    }
-}
-
-
-exports.updateLink = async (req, res) => {
-    const { link, serie_id, title } = req.body;
-
-    if (!serie_id){
-        return res.status(400).send({message: "serie_id is required to delete a link"});
-    }
-
-    if (!link){
-        return res.status(404).send({status: "NOT FOUND", message: "A link is require"});
-    }
-    const linkk = link.trim();
-    try{
-        const titleLower = title.trim().toLowerCase()
-        const responseAllSeries = await queryToGetAllTitles();
-        const serieExists = responseAllSeries.some(serie => {
-            return serie.title === titleLower;
-        });
-
-        if (!serieExists) {
-            return res.status(404).send({ status: "NOT FOUND", message: "That title doesn't exist"});
-        }
-
-        const responseQueryUpdate = await queryUpdateLink(linkk, serie_id);
-        if (responseQueryUpdate. affectedRows === 0){
-            return res.status(400).send({message: "No link was updated"});
-        }
-        
-        return res.status(200).send({
-            status: "Success",
-            message: "Link updated successfully",
-            data: {
-                title: titleLower,
-                New_link: linkk
-            }
-        });
-
-    } catch(error){
-        console.error("Error:", error);
-        return res.status(500).send({message: "Internal server error"});
-    }
-}
-
-
 
 exports.incrementVoteCount = async (req, res) => {
     const { title } = req.body;
@@ -254,10 +160,19 @@ exports.renderSeriesPage = async (req, res) => {
 
 exports.renderEditSeriesPage = async (req, res) => {
     const { serie_id } = req.params;
+
+    console.log("Serie id for update:", serie_id);
     try {
-        const serie = await queryGetSerieById(serie_id);
-        res.render('editSeries', { serie });
+        const serie = await querySerieById(serie_id);
+        console.log("Datos de objeto serie:", serie);
+        
+        if (!serie) {
+            return res.status(404).send("Serie no encontrada");
+        }
+        
+        res.render('editSerie', { serie });
     } catch (error) {
-        res.status(500).send("Error al cargar la página de edición");
+        console.error("Error loading the edition page:", error);
+        return res.status(500).send("Error updating the serie", serie_id);
     }
 };
