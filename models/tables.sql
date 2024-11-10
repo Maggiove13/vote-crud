f0aeae820822dc0a54aee79ad6a8091cdebe48e8
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-10 11:03:09 -0300

    update scrypt to create the tables seasons and series

--- models/tables.sql
+++ models/tables.sql
@@ -1,54 +1,22 @@
 
--- Table users
-
-CREATE TABLE `world`.`users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `user_name` VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC) VISIBLE);
-
-    ALTER TABLE `world`.`users` 
-    ADD COLUMN `password` VARCHAR(255) NOT NULL AFTER `user_name`,
-    ADD COLUMN `email` VARCHAR(255) NOT NULL AFTER `password`,
-    ADD UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE;
-    ;
-
-
 -- Table Series
-    ALTER TABLE `world`.`series` 
-    ADD CONSTRAINT `user_id`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `world`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION;
-
-
-        ALTER TABLE `world`.`series` 
-    ADD COLUMN `season_id` INT NOT NULL AFTER `user_id`,
-    ADD INDEX `season_id_idx` (`season_id` ASC) VISIBLE;
-
-
+    CREATE TABLE `series` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `title` varchar(255) NOT NULL,
+        `description` varchar(255) DEFAULT NULL,
+        `vote_count` int DEFAULT '0', 
+        `link_url` varchar(255) DEFAULT NULL,
+        `image` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`id`)
+    )
 
 -- Table Seasons
-CREATE TABLE `world`.`seasons` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `season_name` VARCHAR(255) NULL,
-    `vote_count` INT NULL,
-    `user_id` INT NULL,
-    `serie_id` INT NULL,
-    PRIMARY KEY (`id`),
-    INDEX `user_id_idx` (`user_id` ASC) VISIBLE,
-    INDEX `serie_id_idx` (`serie_id` ASC) VISIBLE,
-    CONSTRAINT `user_id`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `world`.`users` (`id`)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
-    CONSTRAINT `serie_id`
-        FOREIGN KEY (`serie_id`)
-        REFERENCES `world`.`series` (`id`)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE);
-
-    ALTER TABLE `world`.`seasons` 
-    ADD COLUMN `link_serie` VARCHAR(255) NULL AFTER `serie_id`;
+    CREATE TABLE `seasons` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `season_number` int NOT NULL,
+        `vote_count` int DEFAULT '0',
+        `serie_id` int NOT NULL,
+        `link` varchar(255) DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`serie_id`) REFERENCES `series` (`id`)
+    ) 
