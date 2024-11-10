aa104b965a8d26c8368e57c88f88e3e0add51b6f
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-10 00:23:29 -0300

    delete the innecesary function insertSeason

--- models/seasons.js
+++ models/seasons.js
@@ -1,65 +1,8 @@
 const pool = require("../config/config.js");
 
 
-exports.queryInsertSeason = async (serie_id, user_id, season_name) => {
-    const query = 'INSERT INTO seasons (serie_id, user_id, season_name) VALUES (?, ?, ?)';
-
-    try{
-        const [response] = await pool.execute(query, [serie_id, user_id, season_name]);
-        console.log(response);
-        return response;
-    } catch (error) {
-        console.error("Data could not be inserted. Error:", error);
-        throw error;
-    }
-}
-
-
-//Para que el usuario pueda eliminar una season 
-exports.queryDeleteSeason = async (season_id, serie_id, user_id) => {
-    const query = 'DELETE FROM seasons WHERE id = ? AND serie_id = ? AND user_id = ?'
-
-    try {
-        const [response] = await pool.execute(query, [season_id, serie_id, user_id]);
-        console.log("Season successfully deleted");
-        return response;
-    } catch(error) {
-        console.log("Error deleting the season", error);
-    }
-}
-
-
-//Para que el usuario pueda actualizar el nombre de una season
-exports.queryUpdateSeason = async (season_name, season_id, serie_id, user_id) => {
-    const query = 'UPDATE seasons SET season_name = ? WHERE id = ? AND serie_id = ? AND user_id = ?'
-
-    try {
-        const [response] = await pool.execute(query, [season_name, season_id, serie_id, user_id]);
-        console.log("Season name, successfully deleted");
-        return response;
-    } catch(error) {
-        console.log("Error deleting the season", error);
-    }
-}
-
-
-//Para que el usuario pueda actualizar el nombre de una season
-exports.queryGetSeasonsFromSerie = async (serie_id) => {
-    const query = 'SELECT * FROM seasons WHERE serie_id = ?'
-
-    try {
-        const [response] = await pool.execute(query, [serie_id]);
-        console.log("All seasons retrieved correctly");
-        return response;
-    } catch(error) {
-        console.log("Error retrieving data from seasons table", error);
-    }
-}
-
-
-// Para agregar los votos del usuario a la tabla
 exports.queryVoteCountSeasons = async (season_id) => {
-    const query = 'UPDATE series SET vote_count =  vote_count + 1 WHERE id = ?';
+    const query = 'UPDATE seasons SET vote_count =  vote_count + 1 WHERE id = ?';
     try{
         await pool.execute(query, [season_id]);
         console.log("Vote count incremented successfully in seasons");
