87a10ddaf0bedf4e95152340e9a35fdf2ad2f58b
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-10 00:32:33 -0300

    create a function to render the list of seasons for the clientSide

--- controllers/seasonsController.js
+++ controllers/seasonsController.js
@@ -18,3 +18,23 @@ exports.incrementVoteCount = async (req, res) => {
     }
 }
 
+
+exports.renderAllSeasons = async (req, res) => {
+    const { serie_id } = req.params;
+    
+    try {
+        const seasons = await seasonsModels.queryGetAllSeasons(serie_id);
+        console.log("Datos de objeto seasons:", seasons);
+        
+        if (!seasons || seasons.length === 0) {
+            return res.status(404).send("seasons from that id not found");
+        }
+
+        const title = seasons[0].title
+        res.render('allSeasons', { seasons, title });
+
+    } catch (error) {
+        console.error("Error loading the insert season page:", error);
+        return res.status(500).send("Error inserting the season", serie_id);
+    }
+};
