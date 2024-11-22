1aefa04f1bc97dce3e03a19d24c141b034c0e48e
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-22 14:23:20 -0300

    connection to the database of express-session-mysql

--- models/Session.js
+++ models/Session.js
@@ -1,36 +1,86 @@
 const pool = require("../config/config.js");
 
-exports.queryCreateSession = async (user_id) => {
-    const query = 'INSERT INTO sessions (user_id) VALUE (?)';
-    try {
-        const [ response ] = await pool.execute(query, [user_id]);
-        return response;
-    } catch (error) {
-        console.error("Error in queryCreateSession:", error);
-        throw error;
-    }
-}
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 
 
-exports.queryDeleteSession = async (session_id) => {
-    const query = 'DELETE FROM sessions WHERE id = ?';
-    try {
-        const [ response ] = await pool.execute(query, [session_id]);
-        return response;
-    } catch (error) {
-        console.error("Error in queryCreateSession:", error);
-        throw error;
-    }
-}
+// Opciones personalizadas para mapear la tabla
+const options = {
+    schema: {
+        tableName: "sessions", 
+        columnNames: {
+            session_id: "session_id", 
+            expires: "expires", 
+            data: "session_data" 
+        },
+    },
+};
+
+const sessionStore = new MySQLStore(options, pool);
+
+sessionStore.on('session', function(session) {
+    console.log("Session to be stored: ", session);
+});
+
+module.exports = sessionStore;
 
 
-exports.queryGetSession = async (session_id) => {
-    const query = 'SELECT * FROM sessions WHERE id = ?';
-    try {
-        const [ response ] = await pool.execute(query, [session_id]);
-        return response;
-    } catch (error) {
-        console.error("Error in queryCreateSession:", error);
-        throw error;
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// exports.queryCreateSession = async (user_id) => {
+//     const query = 'INSERT INTO sessions (user_id) VALUE (?)';
+//     try {
+//         const [ response ] = await pool.execute(query, [user_id]);
+//         return response;
+//     } catch (error) {
+//         console.error("Error in queryCreateSession:", error);
+//         throw error;
+//     }
+// }
+
+
+// exports.queryDeleteSession = async (session_id) => {
+//     const query = 'DELETE FROM sessions WHERE id = ?';
+//     try {
+//         const [ response ] = await pool.execute(query, [session_id]);
+//         return response;
+//     } catch (error) {
+//         console.error("Error in queryCreateSession:", error);
+//         throw error;
+//     }
+// }
+
+
+// exports.queryGetSession = async (session_id) => {
+//     const query = 'SELECT * FROM sessions WHERE id = ?';
+//     try {
+//         const [ response ] = await pool.execute(query, [session_id]);
+//         return response;
+//     } catch (error) {
+//         console.error("Error in queryCreateSession:", error);
+//         throw error;
+//     }
+// }
