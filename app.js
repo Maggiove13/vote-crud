905c83004a868a5542c62df0c97823c6e0384bd0
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-23 14:34:45 -0300

    config the csrf token middleware

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ const dotenv = require("dotenv");
 const router = require("./routes/routes.js");
 const { checkDatabaseConnection } = require("./models/models.js"); 
 const cookieParser = require('cookie-parser');
+const Auth = require('./middleware/authorization.js');
+const session = require("express-session");
+const sessionStore = require("./models/Session.js");
+const csurf = require('csurf');
+const xss = require('xss-clean');
 
 dotenv.config()
 
 const app = express();
+const csurfProtection = csurf({cookie: true});
 
 checkDatabaseConnection();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
-
 //app.use(express.static("public"));
+app.use(csurfProtection);
+
+app.use((req, res, next) => {
+    if (req.csrfToken) {
+        res.locals.csrfToken = req.csrfToken(); 
+    }
+    next();
+});
 
 app.set("view engine", "ejs");
 
