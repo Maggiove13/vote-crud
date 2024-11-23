4fdfabe456f1e8d0a440411258031ac7c8419fbc
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-23 14:41:51 -0300

    create the session database connection

---
+++ models/Session.js
@@ -0,0 +1,25 @@
+const pool = require("../config/config.js");
+
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+
+
+// Opciones personalizadas para mapear la tabla
+const options = {
+    schema: {
+        tableName: "sessions", 
+        columnNames: {
+            session_id: "session_id", 
+            expires: "expires", 
+            data: "session_data" 
+        },
+    },
+};
+
+const sessionStore = new MySQLStore(options, pool);
+
+sessionStore.on('session', function(session) {
+    console.log("Session to be stored: ", session);
+});
+
+module.exports = sessionStore;
