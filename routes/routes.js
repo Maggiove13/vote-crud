1eb03a46cb109752e3eeaf36de7ec3a426748b49
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-23 14:48:52 -0300

    add the middlewares to the protected routes

--- routes/routes.js
+++ routes/routes.js
@@ -10,33 +10,17 @@ router.post('/register', userController.registerUser );
 router.post('/login', Auth.limiter, userController.login);
 router.post('/logout', userController.logout);
 
-// Ruta para insertar una nueva serie
-router.post('/series/create', seriesController.insertSerie);
 
-// Ruta para incrementar el contador de votos de una serie
-router.post('/series/vote', seriesController.incrementVoteCount);
-
-// Ruta para eliminar una serie
-router.post('/series/delete', seriesController.deleteSerie);
-
-// Ruta para actualizar una serie específica
-router.post('/series/update/:serie_id', seriesController.updateSerie);
-
-router.post('/series/seasons/all/:serie_id/vote', seasonsController.incrementVoteCount);
+router.post('/series/create',  Auth.requireAdmin, seriesController.insertSerie);
+router.post('/series/vote',  seriesController.incrementVoteCount);
+router.post('/series/delete',  Auth.requireAdmin, seriesController.deleteSerie);
+router.post('/series/update/:serie_id',  Auth.requireAdmin, seriesController.updateSerie);
+router.post('/series/seasons/all/:serie_id/vote',  seasonsController.incrementVoteCount);
 
 //----------------->   Rutas para vistas
-
-// Ruta para mostrar la página de edición de una serie específica
-router.get('/series/edit/:serie_id', seriesController.renderEditSeriesPage);
-
-// Ruta para listar todas las series
-router.get('/series', seriesController.renderSeriesPage);
-
+router.get('/series/edit/:serie_id',  Auth.requireAdmin, seriesController.renderEditSeriesPage);
 router.get('/series/ordered', seriesController.orderedSeries);
-
-// Ruta para Visualizar la pagina para agregar series
-router.get('/series/insert', seriesController.renderAddSeriePage);
-
+router.get('/series/insert',  Auth.requireAdmin, seriesController.renderAddSeriePage);
 router.get('/series/seasons/:serie_id', seasonsController.renderAllSeasons);
 
 
