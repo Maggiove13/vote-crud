a8da09889acf5b7e653306eae7ff217793ec1465
Maggi Ovelar <ovelar.romidesk@gmail.com>
2024-11-23 14:32:36 -0300

    add the cookie-Parser middleware

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 checkDatabaseConnection();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
 
 //app.use(express.static("public"));
